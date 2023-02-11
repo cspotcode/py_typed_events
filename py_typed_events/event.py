aa856f856b422bd97e57a79a91144dfd81fe72e1
Andrew Bradley <cspotcode@gmail.com>
2023-02-10 23:59:19 -0500

    try it

--- py_typed_events/event.py
+++ py_typed_events/event.py
@@ -1,77 +1,68 @@
 from __future__ import annotations
-from inspect import ismethod
 
+from inspect import ismethod
 from typing import Any, Callable, Generic, ParamSpec, TypeVar
-from functools import partial
 from weakref import WeakMethod
 
 P = ParamSpec("P")
 C = TypeVar("C", bound=Callable)
 
+
 class Event(Generic[P]):
     """
-    Stores (weak) references to zero or more subscribers, which are functions
+    Stores (weak) references to zero or more listeners, which are functions
     or methods.
 
-    When emitted, calls each subscriber in order, passing all parameters.
+    When emitted, calls each listener in order, passing all parameters.
     """
 
-    __slots__ = ('_subscribers',)
+    __slots__ = ("_listeners",)
 
     def __init__(self):
         # Note for future: we can save memory by lazily initializing this upon
         # first subscription. See:
         # https://github.com/pyglet/pyglet/blob/d89fa4466d7f5d2183eac91f4ab2bb5fb4db745e/pyglet/event.py#L140-L141
-        self._subscribers: list[WeakMethod | Callable] = []
 
-    def append_listener(self, handler: Callable[P, Any]):
-        return self.__iadd__(handler)
+        # Stored as a tuple to optimize for emitting often, rarely changing listeners
+        self._listeners: tuple[WeakMethod | Callable, ...] = ()
 
-    def __iadd__(self, handler: Callable[P, Any]):
-        subscription = WeakMethod(handler, self._remove_weak_method) if ismethod(handler) else handler # type: ignore
-        self._subscribers.append(subscription)
+    def append_listener(self, listener: Callable[P, Any]):
+        """
+        Subscribe to an event. The listener will be called every time the event is emitted.
+        """
+        l = WeakMethod(listener, self._remove_weak_method) if ismethod(listener) else listener  # type: ignore
+        self._listeners += (l,)
         return self
 
-    def remove_listener(self, handler: Callable[P, Any]):
-        return self.__isub__(handler)
+    __iadd__ = append_listener
 
-    def __isub__(self, handler: Callable[P, Any]):
-        # Iterate over a copy as we might mutate the list
-        for subscription in list(self._subscribers):
-            try:
-                if isinstance(subscription, WeakMethod):
-                    if subscription() == handler:
-                        self._subscribers.remove(subscription)
-                        break
-                else:
-                    if subscription == handler:
-                        self._subscribers.remove(subscription)
-                        break
-            except TypeError:
-                # weakref is already dead
-                pass
+    def remove_listener(self, listener: Callable[P, Any]):
+        """
+        Remove a previously-subscribed event listener.
+        """
+        # Find the last -- most recently subscribed -- listener and remove it.
+        # for i in range(len(self._listeners) - 1, -1, -1):
+        for index_reversed, l in enumerate(reversed(self._listeners)):
+            l_unwrapped = l() if isinstance(l, WeakMethod) else l
+            if l_unwrapped == listener:
+                # Found it
+                i = len(self._listeners) - index_reversed - 1
+                self._listeners = self._listeners[:i] + self._listeners[i + 1 :]
+                break
         return self
+
+    __isub__ = remove_listener
 
     def _remove_weak_method(self, weak_method: WeakMethod) -> object:
-        """
-        Very similar to __isub__, but only used internally in WeakMethod
-        finalizers.  If an instance has subscribed to us, but then is
-        garbage-collected, we remove the subscriptions.
-        """
-        for subscription in list(self._subscribers):
-            try:
-                if weak_method == subscription:
-                    self._subscribers.remove(weak_method)
-                    return
-            except TypeError:
-                # weakref is already dead
-                pass
+        # Very similar to remove_listener, but only used internally in WeakMethod
+        # finalizers.  If an instance has subscribed to us, but then is
+        # garbage-collected, we remove the subscriptions.
+        self._listeners = tuple(l for l in self._listeners if l is not weak_method)
 
     def emit(self, *args: P.args, **kwargs: P.kwargs) -> None:
-        for subscription in self._subscribers:
-            # Unwrap WeakMethod
-            handler = subscription() if isinstance(subscription, WeakMethod) else subscription
-            handler(*args, **kwargs) # type: ignore
+        # No docstring so that consumer's declared docstring surfaces.
+        for l in self._listeners:
+            l_unwrapped = l() if isinstance(l, WeakMethod) else l
+            l_unwrapped(*args, **kwargs)  # type: ignore
 
-    def __call__(self, *args: P.args, **kwargs: P.kwargs) -> None:
-        self.emit(*args, **kwargs)
+    __call__ = emit
--- py_typed_events/event_descriptor.py
+++ py_typed_events/event_descriptor.py
@@ -1,15 +1,19 @@
-from typing import Any, Callable, Concatenate, Generic, ParamSpec, TypeVar, cast
+from __future__ import annotations
+
+from typing import Any, Callable, Concatenate, Generic, ParamSpec, cast
+
 from .event import Event
 
 P = ParamSpec("P")
 
+
 def event(signature: Callable[Concatenate[Any, P], None]):
-    # Lie to the typechecker.
     # In reality, we are returning an `EventDescriptor`.
     # But as soon as anyone tries to access it, it triggers `__get__` and
     # replaces itself with an `Event`.
     # So as far as the consumer is aware, they are *always* dealing with an `Event`.
-    return cast(Event[P], EventDescriptor(signature))
+    return cast(Event[P], EventDescriptor[P]())
+
 
 class EventDescriptor(Generic[P]):
     """
@@ -20,12 +24,15 @@ class EventDescriptor(Generic[P]):
 
     If you want to understand how events work, better to skip this class and look at `Event`.
     """
-    __slots__ = ('_attr_name')
 
-    def __init__(self, signature: Callable[Concatenate[Any, P], None]):
-        self._attr_name = signature.__name__
+    __slots__ = "_attr_name"
+
+    def __set_name__(self, owner, name: str):
+        self._attr_name = name
 
     def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
         # Create `Event` instance on-demand and attach to the instance.
         event = Event[P]()
         setattr(obj, self._attr_name, event)
